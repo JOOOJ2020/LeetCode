6db6896488bd2efc54e100a4b8156cdb9a0314d1
JOOOJ2020 <JOOOJ2020@users.noreply.github.com>
2016-06-27 21:59:15 +0800

    Complete LeetCode 98: Validate Binary Search Tree

---
+++ LeetCode/ValidateBinarySearchTree.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    /* https://leetcode.com/problems/validate-binary-search-tree/
+     * 98. Validate Binary Search Tree
+        Total Accepted: 98596 Total Submissions: 465688 Difficulty: Medium
+        Given a binary tree, determine if it is a valid binary search tree (BST).
+
+        Assume a BST is defined as follows:
+
+        The left subtree of a node contains only nodes with keys less than the node's key.
+        The right subtree of a node contains only nodes with keys greater than the node's key.
+        Both the left and right subtrees must also be binary search trees.
+        Example 1:
+            2
+           / \
+          1   3
+        Binary tree [2,1,3], return true.
+        Example 2:
+            1
+           / \
+          2   3
+        Binary tree [1,2,3], return false.
+     */
+    /**
+    * Definition for a binary tree node.
+    * public class TreeNode {
+    *     public int val;
+    *     public TreeNode left;
+    *     public TreeNode right;
+    *     public TreeNode(int x) { val = x; }
+    * }
+    */
+    internal class ValidateBinarySearchTree
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            if (root == null)
+            {
+                return true;
+            }
+            TreeNode lastNode = null;
+            while (stack.Count > 0 || root != null)
+            {
+                if (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+                else
+                {
+                    root = stack.Pop();
+                    if (lastNode == null)
+                    {
+                        lastNode = root;
+                    }
+                    else
+                    {
+                        if (lastNode.val >= root.val)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            lastNode = root;
+                        }
+                    }
+                    root = root.right;
+                }
+            }
+            return true;
+        }
+
+    }
+}
