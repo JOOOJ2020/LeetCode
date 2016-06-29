7ccf1a0bc0b9a1182df12cd0c14a7364e0eb4f71
JOOOJ2020 <JOOOJ2020@users.noreply.github.com>
2016-06-29 13:35:08 +0800

    Complete LeetCode 137:Single Number II

---
+++ LeetCode/SingleNumberII.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetCode
+{
+    /* https://leetcode.com/problems/single-number-ii/
+     * 137. Single Number II 
+        Total Accepted: 87716 Total Submissions: 229174 Difficulty: Medium
+        Given an array of integers, every element appears three times except for one. Find that single one.
+
+        Note:
+        Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+     */
+    internal class SingleNumberII
+    {
+        public int Solution(int[] nums)
+        {
+            if(nums==null)
+            {
+                throw new ArgumentNullException();
+            }
+            int[] bits = new int[32];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < 32; j++)
+                {
+                    int temp = (int)Math.Pow(2, j);
+                    if((nums[i]&temp)==temp)
+                    {
+                        bits[31 - j] += 1;
+                    }
+                }
+            }
+            for (int i = 0; i < 32; i++)
+            {
+                if(bits[i] !=0 )
+                {
+                    bits[i] = bits[i]%3;
+                }
+            }
+            string str = string.Join("", bits);
+            return Convert.ToInt32(str, 2);
+
+        }
+    }
+}
