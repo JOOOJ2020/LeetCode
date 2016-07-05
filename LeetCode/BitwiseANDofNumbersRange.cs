b50793625ef597f823ad5333b58e62ee50a939f6
JOOOJ2020 <JOOOJ2020@users.noreply.github.com>
2016-07-05 14:32:15 +0800

    Complete LeetCode 201:Bitwise AND of Numbers Range

---
+++ LeetCode/BitwiseANDofNumbersRange.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode
+{
+    /* https://leetcode.com/problems/bitwise-and-of-numbers-range/
+     * 201. Bitwise AND of Numbers Range
+        Total Accepted: 37407 Total Submissions: 119455 Difficulty: Medium
+        Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+        For example, given the range [5, 7], you should return 4.
+     */
+    internal class BitwiseANDofNumbersRange
+    {
+        public int RangeBitwiseAnd(int m, int n)
+        {
+            //The idea is very simple:
+            //last bit of(odd number & even number) is 0.
+            //when m != n, There is at least an odd number and an even number, so the last bit position result is 0.
+            //Move m and n rigth a position.
+            //Keep doing step 1,2,3 until m equal to n, use a factor to record the iteration time.
+            //How many bit need be move from right to left between m and n. For instance, 5=101, 7=111. 101!=111, 10!=11,1==1
+            int res = 0;
+            while(m!=n)
+            {
+                res++;
+                m = m >> 1;
+                n = n >> 1;
+            }
+            // now m=n, and n or m has move res steps to right, now move res steps back (move left).
+            return n << res;
+        }
+    }
+}
---
+++ LeetCode/NumberOf1Bits.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode
+{
+    /* https://leetcode.com/problems/number-of-1-bits/
+     * 191. Number of 1 Bits
+        Total Accepted: 99580 Total Submissions: 264891 Difficulty: Easy
+        Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+        For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+     */
+    internal class NumberOf1Bits
+    {
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+            while(n>0)
+            {
+                count++;
+                n = n & (n - 1);
+            }
+            return count;
+        }
+    }
+}
