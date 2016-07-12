c83bb15298697bc9f36629e34f684786528c4d26
JOOOJ2020 <JOOOJ2020@users.noreply.github.com>
2016-07-12 10:41:16 +0800

    Complete LeetCode 231:Power of Two

---
+++ LeetCode/PowerOfTwo.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    /* https://leetcode.com/problems/power-of-two/
+     * 231. Power of Two
+        Total Accepted: 84672
+        Total Submissions: 224665
+        Difficulty: Easy
+        Given an integer, write a function to determine if it is a power of two.
+     */
+    internal class PowerOfTwo
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if(n==0 || n==int.MinValue)
+            {
+                return false;
+            }
+            return (n & (n - 1)) == 0;
+        }
+    }
+}
