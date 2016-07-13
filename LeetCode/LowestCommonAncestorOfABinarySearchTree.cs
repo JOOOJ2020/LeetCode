41775c0dc03b9c518208e4beee4adaa490911227
JOOOJ2020 <JOOOJ2020@users.noreply.github.com>
2016-07-13 09:35:53 +0800

    Complete LeetCode 235:Lowest Common Ancestor of a Binary Search Tree

---
+++ LeetCode/LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode
+{
+    /* https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+     * 235. Lowest Common Ancestor of a Binary Search Tree
+        Total Accepted: 79319 Total Submissions: 211420 Difficulty: Easy
+        Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+        According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+                _______6______
+               /              \
+            ___2__          ___8__
+           /      \        /      \
+           0      _4       7       9
+                 /  \
+                 3   5
+        For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+     */
+    /**
+    * Definition for a binary tree node.
+    * public class TreeNode {
+    *     public int val;
+    *     public TreeNode left;
+    *     public TreeNode right;
+    *     public TreeNode(int x) { val = x; }
+    * }
+    */
+    internal class LowestCommonAncestorOfABinarySearchTree
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if ((p.val <= root.val && q.val >= root.val) || (p.val >= root.val && q.val <= root.val))
+                return root;
+            else if(p.val < root.val && q.val < root.val)
+            {
+                return LowestCommonAncestor(root.left, p, q);
+            }
+            else
+            {
+                return LowestCommonAncestor(root.right, p, q);
+            }
+        }
+    }
+}
