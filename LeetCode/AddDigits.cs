8e7f3e1f0107417c64f5ef0df6f3099fa5b4521a
JOOOJ2020 <JOOOJ2020@user.noreply.github.com>
2016-07-17 23:20:03 +0800

    Complete LeetCode 258:Add Digits

---
+++ LeetCode/AddDigits.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode
+{
+    /* https://leetcode.com/problems/add-digits/
+     * 258. Add Digits
+        Total Accepted: 109812
+        Total Submissions: 223122
+        Difficulty: Easy
+        Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+        For example:
+
+        Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+        Follow up:
+        Could you do it without any loop/recursion in O(1) runtime?
+
+        Hint:
+
+        A naive implementation of the above process is trivial. Could you come up with other methods?
+        What are all the possible results?
+        How do they occur, periodically or randomly?
+        You may find this Wikipedia article useful.
+     */
+    internal class AddDigits
+    {
+        public int Solution(int num)
+        {
+            if(num < 10)
+            {
+                return num;
+            }
+            else if(num%9==0)
+            {
+                return 9;
+            }
+            return num % 9;
+        }
+    }
+}
