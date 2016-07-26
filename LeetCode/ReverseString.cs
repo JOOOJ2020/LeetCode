eba3eebf6b357bae9492060d9ddb7314c6c4e9d2
JOOOJ2020 <JOOOJ2020@users.noreply.github.com>
2016-07-26 10:49:49 +0800

    Complete LeetCode 344:Reverse String

---
+++ LeetCode/ReverseString.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode
+{
+    /* https://leetcode.com/problems/reverse-string/
+     * 344. Reverse String
+        Total Accepted: 58791
+        Total Submissions: 100171
+        Difficulty: Easy
+        Write a function that takes a string as input and returns the string reversed.
+
+        Example:
+        Given s = "hello", return "olleh".
+     */
+    internal class ReverseString
+    {
+        public string Solution(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+            char[] c = s.ToCharArray();
+            int i = 0, j = c.Length - 1;
+            while(i<j)
+            {
+                char temp = c[i];
+                c[i] = c[j];
+                c[j] = temp;
+                i++;
+                j--;
+            }
+            return new string(c);
+        }
+    }
+}
