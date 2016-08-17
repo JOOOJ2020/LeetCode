c56f985255e673dd14ef019f53323510eceb0e2f
JOOOJ2020 <JOOOJ2020@users.noreply.github.com>
2016-08-17 10:07:39 +0800

    Complete LeetCode 69:Sqrt(x)

---
+++ LeetCode/Sqrt.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode
+{
+    /* https://leetcode.com/problems/sqrtx/
+     * 69. Sqrt(x)
+        Total Accepted: 106308
+        Total Submissions: 408313
+        Difficulty: Medium
+        Implement int sqrt(int x).
+
+        Compute and return the square root of x.
+     */
+    internal class Sqrt
+    {
+        public int MySqrt(int x)
+        {
+            if(x<2)
+            {
+                return x;
+            }
+            long i = 0, j = x / 2;
+            while(i<=j)
+            {
+                long mid = i + (j - i) / 2;
+                if(mid*mid==x)
+                {
+                    return (int)mid;
+                }
+                else if(mid*mid>x)
+                {
+                    j = mid - 1;
+                }
+                else
+                {
+                    i = mid + 1;
+                }
+            }
+            return (int)j;
+        }
+    }
+}
