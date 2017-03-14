53e1be084a26ec625b914924496f6ce6128f5799
JOOOJ2020 <JOOOJ2020@users.noreply.github.com>
2017-03-14 10:43:00 +0800

    Add LeetCode 413 Arithmetic Slice

---
+++ LeetCode/ArithmeticSlices.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCode
+{
+    /* https://leetcode.com/problems/arithmetic-slices/#/description
+     * 413. Arithmetic Slices
+       
+        Total Accepted: 16219
+        Total Submissions: 29644
+        Difficulty: Medium
+        Contributors: XiangyuLi926
+        A sequence of number is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+        For example, these are arithmetic sequence:
+
+        1, 3, 5, 7, 9
+        7, 7, 7, 7
+        3, -1, -5, -9
+        The following sequence is not arithmetic.
+
+        1, 1, 2, 5, 7
+
+        A zero-indexed array A consisting of N numbers is given. A slice of that array is any pair of integers (P, Q) such that 0 <= P < Q < N.
+
+        A slice (P, Q) of array A is called arithmetic if the sequence:
+        A[P], A[p + 1], ..., A[Q - 1], A[Q] is arithmetic. In particular, this means that P + 1 < Q.
+
+        The function should return the number of arithmetic slices in the array A.
+        Example:
+        A = [1, 2, 3, 4]
+
+        return: 3, for 3 arithmetic slices in A: [1, 2, 3], [2, 3, 4] and [1, 2, 3, 4] itself.
+     */
+    internal class ArithmeticSlices
+    {
+        public int NumberOfArithmeticSlices(int[] A)
+        {
+            if (A == null)
+            {
+                return 0;
+            }
+            int cur = 0, sum = 0;
+            for (int i = 2; i < A.Length; i++)
+            {
+                if ((A[i] - A[i - 1]) == (A[i - 1] - A[i - 2]))
+                {
+                    cur += 1;
+                    sum += cur;
+                }
+                else
+                {
+                    cur = 0;
+                }
+            }
+            return sum;
+        }
+    }
+}
